707bdcf3bc940504dd5683d1dc6368b963563552
Jorel Fermin <jorelius@gmail.com>
2019-04-17 07:14:41 -0400

    add count to simple request arguments

--- src/Fenrir.Cli/SimpleArgs.cs
+++ src/Fenrir.Cli/SimpleArgs.cs
@@ -8,7 +8,10 @@ namespace Fenrir.Cli
         [ArgDescription("number of parallel threads to use"), ArgShortcut("t"), DefaultValue(1), ArgRange(1, int.MaxValue)]
         public int Concurrency { get; set; }
 
-        [ArgDescription("length of time to run load test"), ArgShortcut("d"), DefaultValue("00:00:30")]
+        [ArgDescription("number requests to make"), ArgShortcut("c"), DefaultValue(-1), ArgRange(1, int.MaxValue)]
+        public int Count { get; set; }
+
+        [ArgDescription("length of time to run load test"), ArgShortcut("d"), DefaultValue("00:00:30"), ArgIgnore]
         public TimeSpan Duration { get; set; }
 
         [ArgRequired, ArgDescription("load test url"), ArgShortcut("url"), ArgPosition(3)]
