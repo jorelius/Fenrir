050faf30d5f6e09c369de6b3cb48a1ba0ebba3ec
Jorel Fermin <jorelius@gmail.com>
2019-08-03 09:43:05 -0400

    add start time to request stats

--- src/Fenrir.Core/Jobs/HttpClientAgentJob.cs
+++ src/Fenrir.Core/Jobs/HttpClientAgentJob.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -10,51 +11,49 @@ namespace Fenrir.Core.Jobs
 {
     public class HttpClientAgentJob : IAgentJob
     {
-        private readonly HttpRequestMessage _request;
+        private readonly Request _request;
         private readonly Stopwatch _localStopwatch;
-        private readonly AgentThreadResult _agentThreadResult;
         private readonly HttpClient _httpClient;
 
-        public HttpClientAgentJob(HttpClient httpClient, HttpRequestMessage request, AgentThreadResult agentThreadResult)
-        {      
-            _agentThreadResult = agentThreadResult;
+
+        public HttpClientAgentJob(HttpClient httpClient, Request request) 
+        {
             _request = request;
             _httpClient = httpClient;
             _localStopwatch = new Stopwatch();
         }
 
-        public HttpClientAgentJob(HttpClient httpClient, Request request) : this(httpClient, request.ToHttpRequestMessage(), new AgentThreadResult(request))
-        {
-
-        }
-
         public async Task<AgentThreadResult> DoWork()
         {
+            AgentThreadResult threadResult = new AgentThreadResult(_request);
+            HttpRequestMessage httpRequest = _request.ToHttpRequestMessage();
+
+            DateTime startTime = DateTime.Now.ToLocalTime();
             _localStopwatch.Restart();
             
             try 
             {
-                using (var response = await _httpClient.SendAsync(_request).ConfigureAwait(false))
+                using (var response = await _httpClient.SendAsync(httpRequest).ConfigureAwait(false))
                 {
                     var contentStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
                     var length = contentStream.Length + response.Headers.ToString().Length;
-                    
-                    _agentThreadResult.AddResult(await response.ToResult().ConfigureAwait(false));
+
+                    threadResult.AddResult(await response.ToResult().ConfigureAwait(false));
 
                     var code = (int)response.StatusCode;
                     if ((int)response.StatusCode < 400)
-                        _agentThreadResult.Add(length, _localStopwatch.ElapsedMilliseconds, code);
+                        threadResult.Add(length, _localStopwatch.ElapsedMilliseconds, startTime, code);
                     else
-                        _agentThreadResult.AddError(_localStopwatch.ElapsedMilliseconds, code);
+                        threadResult.AddError(_localStopwatch.ElapsedMilliseconds, startTime, code);
                 }
             } 
             catch (HttpRequestException e) // server may fail to respond because of load 
             {
-                _agentThreadResult.AddResult(new Result { Code = -1 });
-                _agentThreadResult.AddError(_localStopwatch.ElapsedMilliseconds, -1);
+                threadResult.AddResult(new Result { Code = -1 });
+                threadResult.AddError(_localStopwatch.ElapsedMilliseconds, startTime, -1);
             }
 
-            return _agentThreadResult;
+            return threadResult;
         }
     }
 }
--- src/Fenrir.Core/Models/RequestStats.cs
+++ src/Fenrir.Core/Models/RequestStats.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Fenrir.Core.Models
@@ -9,18 +10,22 @@ namespace Fenrir.Core.Models
         public float ResponseTime { get; private set; } = 0;
         public int StatusCode { get; private set; } = -1;
 
+        public DateTime StartTime { get; private set; }
 
-        public void Add(long bytes, float responseTime, int statusCode)
+
+        public void Add(long bytes, float responseTime, DateTime startTime, int statusCode)
         {
             Bytes = bytes;
             ResponseTime = responseTime;
+            StartTime = startTime;
             StatusCode = statusCode;
         }
 
-        public void AddError(float responseTime, int statusCode)
+        public void AddError(float responseTime, DateTime startTime, int statusCode)
         {
             isError = true;
             ResponseTime = responseTime;
+            StartTime = startTime;
             StatusCode = statusCode;
         }
     }
