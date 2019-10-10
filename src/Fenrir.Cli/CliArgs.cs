ad3643c3e276b372e04dbc61c46b3ac621f62d3d
Jorel Fermin <jfermin@cimpress.com>
2019-10-10 15:35:26 -0400

    fix result returned

--- src/Fenrir.Cli/CliArgs.cs
+++ src/Fenrir.Cli/CliArgs.cs
@@ -156,7 +156,7 @@ namespace Fenrir.Cli
                     WriteIndented = true
                 };
 
-                await JsonSerializer.SerializeAsync<AgentResult>(stream, requestResult, options);
+                await JsonSerializer.SerializeAsync<HttpRequestTree>(stream, result, options);
             }
         }
 
