2dc18e7f41839a32f5bdc5d0634117576d1d7ce5
Jorel Fermin <jorelius@gmail.com>
2020-05-07 10:12:15 -0400

    encode anything that is not text, json, xml as base64

--- src/Fenrir.Core/Models/RequestTree/Request.cs
+++ src/Fenrir.Core/Models/RequestTree/Request.cs
@@ -4,11 +4,23 @@ namespace Fenrir.Core.Models.RequestTree
 {
     public class Request
     {
+        /// <summary>
+        /// Requests that should be sequenced
+        /// before this one
+        /// </summary>
         public Request[] Pre { get; set; }
         public string Url { get; set; }
         public string Method { get; set; }
         public Payload Payload { get; set; }
+
+        /// <summary>
+        /// Expected Result for request
+        /// </summary>
         public Result ExpectedResult { get; set; }
+
+        /// <summary>
+        /// Metadata for request
+        /// </summary>
         public Metadata Metadata { get; set; }
     }
 }
--- src/Fenrir.Core/Models/RequestTree/Result.cs
+++ src/Fenrir.Core/Models/RequestTree/Result.cs
@@ -2,6 +2,7 @@ namespace Fenrir.Core.Models.RequestTree
 {
     public class Result
     {
+        public string Url { get; set; }
         public int Code { get; set; }
         public Payload Payload { get; set; }
     }
